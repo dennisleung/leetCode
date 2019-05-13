df8a832530ded1273511eb419439412497ef983a
dennisleung <xiyuxistory@163.com>
2019-05-14 00:51:14 +0800

    regular submission

---
+++ 504.base-7.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=504 lang=javascript
+ *
+ * [504] Base 7
+ */
+/*
+✔ Accepted
+  ✔ 241/241 cases passed (44 ms)
+  ✔ Your runtime beats 100 % of javascript submissions
+  ✔ Your memory usage beats 25 % of javascript submissions (33.9 MB)
+
+  javascript内置进制转换方法。。
+*/
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+    return num.toString(7);
+};
+
