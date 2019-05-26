b8d3f55cdf905867b551634cee5fc0f977c5b123
dennisleung <xiyuxistory@163.com>
2019-05-26 15:16:28 +0800

    492莫名其妙做重复了。。

--- 492.construct-the-rectangle.js
+++ 492.construct-the-rectangle.js
@@ -5,19 +5,16 @@
  */
 /*
 ✔ Accepted
-  ✔ 50/50 cases passed (52 ms)
-  ✔ Your runtime beats 100 % of javascript submissions
-  ✔ Your memory usage beats 10 % of javascript submissions (34.7 MB)
+  ✔ 50/50 cases passed (76 ms)
+  ✔ Your runtime beats 41.61 % of javascript submissions
+  ✔ Your memory usage beats 91 % of javascript submissions (34.2 MB)
 
-
-more clean solution:
-var constructRectangle = function(area) {
-  for(let i = Math.floor(Math.sqrt(area)); i > 0 ; i--) {
-    if (area % i == 0) {
-      return [area/i, i];
-    }
-  }
-};
+ 这道题网上的解法都差不多，没搞懂为啥runtime差别那么大。。 
+ 注意这道题求w优于求l，及
+ for(let w = Math.floor(Math.sqrt(area)); w>0; w--) 
+ 优于
+ for(let l = Math.ceil(Math.sqrt(area)); l<=area; l++) 
+ sqrt(area) ~ area必然需要检查更多的元素。
 */
 
 /**
@@ -25,15 +22,10 @@ var constructRectangle = function(area) {
  * @return {number[]}
  */
 var constructRectangle = function(area) {
-    let sqrt = ~~Math.sqrt(area);
-    let cursor = sqrt;
-    while(cursor>=1) {
-        if(area%cursor==0) {
-            return [cursor, area/cursor].sort((a,b)=>b-a);
+    for(let w = Math.floor(Math.sqrt(area)); w>0; w--) {
+        if(area%w==0) {
+            return [area/w, w];
         }
-        cursor--;
     }
-
-    return [area, 1];
 };
 
