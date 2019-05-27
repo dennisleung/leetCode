778254e4ae9c204656a122097903f98a6e3bd4c3
dennisleung <xiyuxistory@163.com>
2019-05-27 23:18:02 +0800

    regular submission

--- 558.quad-tree-intersection.js
+++ 558.quad-tree-intersection.js
@@ -14,12 +14,37 @@
  *    this.bottomRight = bottomRight;
  * };
  */
+/*
+✔ Accepted
+  ✔ 58/58 cases passed (204 ms)
+  ✔ Your runtime beats 21.21 % of javascript submissions
+  ✔ Your memory usage beats 81.48 % of javascript submissions (43.2 MB)
+*/
+
 /**
  * @param {Node} quadTree1
  * @param {Node} quadTree2
  * @return {Node}
  */
 var intersect = function(quadTree1, quadTree2) {
-    
+    if(quadTree1.isLeaf) {
+        return quadTree1.val ? quadTree1 : quadTree2;
+    }
+
+    if(quadTree2.isLeaf) {
+        return quadTree2.val ? quadTree2 : quadTree1;
+    }
+
+    let topLeft = intersect(quadTree1.topLeft, quadTree2.topLeft);
+    let topRight = intersect(quadTree1.topRight, quadTree2.topRight);
+    let bottomLeft = intersect(quadTree1.bottomLeft, quadTree2.bottomLeft);
+    let bottomRight = intersect(quadTree1.bottomRight, quadTree2.bottomRight);
+
+    // Detect a merger state when all 4 leaves have same value.
+    if(topLeft.isLeaf&&topRight.isLeaf&&bottomLeft.isLeaf&&bottomRight.isLeaf && topLeft.val==topRight.val && topRight.val==bottomLeft.val && bottomLeft.val==bottomRight.val) {
+        return new Node(topLeft.val, true, null, null, null, null);
+    }else {
+        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight)
+    }
 };
 
