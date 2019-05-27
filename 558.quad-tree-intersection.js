fa0c3fd76113f236afc3a34ecd22901148893ca0
dennisleung <xiyuxistory@163.com>
2019-05-27 09:40:50 +0800

    temp save, not finished yet

---
+++ 558.quad-tree-intersection.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=558 lang=javascript
+ *
+ * [558] Quad Tree Intersection
+ */
+/**
+ * // Definition for a QuadTree node.
+ * function Node(val,isLeaf,topLeft,topRight,bottomLeft,bottomRight) {
+ *    this.val = val;
+ *    this.isLeaf = isLeaf;
+ *    this.topLeft = topLeft;
+ *    this.topRight = topRight;
+ *    this.bottomLeft = bottomLeft;
+ *    this.bottomRight = bottomRight;
+ * };
+ */
+/**
+ * @param {Node} quadTree1
+ * @param {Node} quadTree2
+ * @return {Node}
+ */
+var intersect = function(quadTree1, quadTree2) {
+    
+};
+
