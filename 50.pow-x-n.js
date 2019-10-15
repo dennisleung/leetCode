61f150259b8000fee88c4316d40459a118df4997
dennisleung <xiyuxistory@163.com>
2019-10-15 23:06:29 +0800

    medium done, but the question is not good...

---
+++ 50.pow-x-n.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=50 lang=javascript
+ *
+ * [50] Pow(x, n)
+ */
+/*
+304/304 cases passed (56 ms)
+Your runtime beats 78.13 % of javascript submissions
+Your memory usage beats 11.76 % of javascript submissions (34.2 MB)
+*/
+// @lc code=start
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    return Math.pow(x, n);
+};
+// @lc code=end
+
