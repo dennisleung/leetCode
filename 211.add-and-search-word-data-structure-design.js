0091bba2e23392e15b3111110582d00f1c2d62ab
dennisleung <xiyuxistory@163.com>
2020-01-04 22:29:17 +0800

    medium done

--- 211.add-and-search-word-data-structure-design.js
+++ 211.add-and-search-word-data-structure-design.js
@@ -3,13 +3,16 @@
  *
  * [211] Add and Search Word - Data structure design
  */
+/*
+这道题没ac，trie的解法不知道为啥一直通不过。。。
+*/
 
 // @lc code=start
 /**
  * Initialize your data structure here.
  */
 var WordDictionary = function() {
-    
+    this.root = {};
 };
 
 /**
@@ -18,7 +21,9 @@ var WordDictionary = function() {
  * @return {void}
  */
 WordDictionary.prototype.addWord = function(word) {
-    
+    const map = this.root[word.length] || {};//巧妙使用长度分组
+    map[word] = 1;
+    this.root[word.length] = map;
 };
 
 /**
@@ -27,7 +32,15 @@ WordDictionary.prototype.addWord = function(word) {
  * @return {boolean}
  */
 WordDictionary.prototype.search = function(word) {
-    
+    const map = this.root[word.length] || {};
+    const reg = new RegExp(`^${word}$`);
+    for(let w in map) {
+        if(reg.test(w)) {
+            return true;
+        }
+    }
+
+    return false;
 };
 
 /** 
@@ -36,5 +49,22 @@ WordDictionary.prototype.search = function(word) {
  * obj.addWord(word)
  * var param_2 = obj.search(word)
  */
+
+// test cases:
+// const dir = new WordDictionary();
+// dir.addWord("bad")
+// dir.addWord("dad")
+// dir.addWord("mad")
+// assertCompare(()=>dir.search("pad"), false);
+// assertCompare(()=>dir.search("bad"), true);
+// assertCompare(()=>dir.search(".ad"), true);
+// assertCompare(()=>dir.search("b.."), true);
+
+// const dir = new WordDictionary();
+// dir.addWord("a")
+// dir.addWord("ab")
+// assertCompare(()=>dir.search("a"), true);
+// assertCompare(()=>dir.search("a."), true);
+
 // @lc code=end
 
